0cf2ca06747511bfe977f636b648990f44049fd5
Ankush Gosal <gosalankush89@gmail.com>
2026-09-21 13:02:47 +0530

    Update Admin Account

--- src/main/java/com/retailpulse/Main.java
+++ src/main/java/com/retailpulse/Main.java
@@ -17,87 +17,89 @@ public class Main {
             int mchoice = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("1. View All Products");
-            System.out.println("2. Add New Product");
-            System.out.println("3. Update Product Details");
-            System.out.println("4. Search Product by Name");
-            System.out.println("5. Low Stock Alerts");
-            System.out.println("6. Sort Products by Price");
-            System.out.println("7. Sort Products by Stocks");
-            System.out.println("8. Delete Product");
-            System.out.println("9. Total Inventory Value Reports");
-            System.out.println("10. Exit");
-            System.out.print("Choose an option: ");
+            if (mchoice == 1){
+                System.out.println("1. View All Products");
+                System.out.println("2. Add New Product");
+                System.out.println("3. Update Product Details");
+                System.out.println("4. Search Product by Name");
+                System.out.println("5. Low Stock Alerts");
+                System.out.println("6. Sort Products by Price");
+                System.out.println("7. Sort Products by Stocks");
+                System.out.println("8. Delete Product");
+                System.out.println("9. Total Inventory Value Reports");
+                System.out.println("10. Exit");
+                System.out.print("Choose an option: ");
             
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+                int choice = scanner.nextInt();
+                scanner.nextLine();
             
-            switch (choice) {
-                case 1:
-                    productDAO.viewProducts();
-                    break;
-                case 2:
-                    System.out.print("Enter product name: ");
-                    String name = scanner.nextLine();
-                    System.out.print("Enter product price: ");
-                    double price = scanner.nextDouble();
-                    System.out.print("Enter stock quantity: ");
-                    int stock = scanner.nextInt();
-                    
-                    productDAO.addProduct(name, price, stock);
-                    break;
-                case 3:
-                    System.out.print("Enter the product's name you want to update: ");
-                    name = scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        productDAO.viewProducts();
+                        break;
+                    case 2:
+                        System.out.print("Enter product name: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Enter product price: ");
+                        double price = scanner.nextDouble();
+                        System.out.print("Enter stock quantity: ");
+                        int stock = scanner.nextInt();
+                        
+                        productDAO.addProduct(name, price, stock);
+                        break;
+                    case 3:
+                        System.out.print("Enter the product's name you want to update: ");
+                        name = scanner.nextLine();
 
-                    System.out.print("Enter new price: ");
-                    double newPrice = scanner.nextDouble();
-                    System.out.print("Enter new stock quantity: ");
-                    int newStock = scanner.nextInt();
-                    scanner.nextLine();
+                        System.out.print("Enter new price: ");
+                        double newPrice = scanner.nextDouble();
+                        System.out.print("Enter new stock quantity: ");
+                        int newStock = scanner.nextInt();
+                        scanner.nextLine();
 
-                    productDAO.updateProducts(name, newPrice, newStock);
-                    break;
-                case 4:
-                    System.out.print("Enter product's name you want to search: ");
-                    name = scanner.nextLine();
+                        productDAO.updateProducts(name, newPrice, newStock);
+                        break;
+                    case 4:
+                        System.out.print("Enter product's name you want to search: ");
+                        name = scanner.nextLine();
 
-                    productDAO.searchProducts(name);
-                    break;
-                case 5:
-                    productDAO.lowstockProducts();
-                    break;
-                case 6:
-                    System.out.println("\nBy which order you want to sort your products 'Ascending or Descending'?");
-                    System.out .print("Enter 'a' for Asc or 'd' for Dsc: ");
-                    String priceorder = scanner.nextLine();
+                       productDAO.searchProducts(name);
+                       break;
+                    case 5:
+                        productDAO.lowstockProducts();
+                        break;
+                    case 6:
+                        System.out.println("\nBy which order you want to sort your products 'Ascending or Descending'?");
+                        System.out .print("Enter 'a' for Asc or 'd' for Dsc: ");
+                        String priceorder = scanner.nextLine();
 
-                    productDAO.priceSort(priceorder);
-                    break;
-                case 7:
-                    System.out.println("\nBy which order you want to sort your products 'Ascending or Descending'?");
-                    System.out .print("Enter 'a' for Asc or 'd' for Dsc: ");
-                    String stockorder = scanner.nextLine();
+                        productDAO.priceSort(priceorder);
+                        break;
+                    case 7:
+                        System.out.println("\nBy which order you want to sort your products 'Ascending or Descending'?");
+                        System.out .print("Enter 'a' for Asc or 'd' for Dsc: ");
+                        String stockorder = scanner.nextLine();
 
-                    productDAO.stockSort(stockorder);
-                    break;
+                        productDAO.stockSort(stockorder);
+                        break;
 
-                case 8:
-                    System.out.print("Enter product's name you want to delete: ");
-                    name = scanner.nextLine();
+                    case 8:
+                        System.out.print("Enter product's name you want to delete: ");
+                        name = scanner.nextLine();
 
-                    productDAO.deleteProducts(name);
-                    break;
-                case 9:
-                    productDAO.totalInventory();
-                    break;
-                case 10:
-                    System.out.println("Exiting application. Goodbye!\n");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid option. Please try again.\n");
-            }
+                        productDAO.deleteProducts(name);
+                        break;
+                    case 9:
+                        productDAO.totalInventory();
+                        break;
+                    case 10:
+                        System.out.println("Exiting application. Goodbye!\n");
+                        scanner.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid option. Please try again.\n");
+                }
+            } 
         }
     }
 }
